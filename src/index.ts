e4c5c4289295d8771c7adb35d502b91b3b148898
William Hogben <will@dave.com>
2019-10-04 11:59:17 -0700

    fix helpers and add logger

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,27 @@ import * as express from 'express';
 import { router } from './router';
 import { handleError } from './middleware/handle-error';
 import * as bodyParser from 'body-parser';
+import { NextFunction, Request, Response } from 'express';
 
 const app = express();
 
 app.use(bodyParser.json({ limit: '10mb' }));
 
-app.get('/', (req, res) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.info({
+    ip: req.ip,
+    method: req.method,
+    url: req.url,
+    query: req.query,
+    payload: req.body,
+    headers: req.headers,
+    endpoint: req.route ? req.route.path : req.originalUrl,
+    logType: 'requestStart',
+  });
+  next();
+});
+
+app.get('/', async (req, res) => {
   return res.send({ ok: true });
 });
 
