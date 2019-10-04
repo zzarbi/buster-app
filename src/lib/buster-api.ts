bcedf28e91ccc6c09614f5ad4e43464e8c95684f
William Hogben <will@dave.com>
2019-10-04 12:51:05 -0700

    use apikey in functions

--- src/lib/buster-api.ts
+++ src/lib/buster-api.ts
@@ -9,22 +9,25 @@ export function createApiKey(webhookUrl: string) {
     .then(res => res.body);
 }
 
-export function createTransaction(referenceId: string) {
+export function createTransaction(referenceId: string, apiKey: string) {
   return request
     .post(`${BUSTER_API_URL}/v1/transaction`)
+    .set({ ['X-API-KEY']: apiKey })
     .send({ referenceId })
     .then(res => res.body);
 }
 
-export function getTransaction(externalId: string) {
+export function getTransaction(externalId: string, apiKey: string) {
   return request
     .get(`${BUSTER_API_URL}/v1/transaction/${externalId}`)
+    .set({ ['X-API-KEY']: apiKey })
     .then(res => res.body);
 }
 
-export function getTransactionByReferenceId(referenceId: string) {
+export function getTransactionByReferenceId(referenceId: string, apiKey: string) {
   return request
     .get(`${BUSTER_API_URL}/v1/transaction`)
+    .set({ ['X-API-KEY']: apiKey })
     .query({ referenceId })
     .then(res => res.body);
 }
